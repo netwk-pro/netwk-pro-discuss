df8977e1c78c1fee98e89fe300ae3608520b3dc5
Scott Lopez <github@sl.neteng.cc>
2025-11-16 04:45:36 -0700

    chore: release prep for v1.0.0 (#2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,6 @@ This file is part of Network Pro.
 
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import mocha from "eslint-plugin-mocha";
 import globals from "globals";
 
 const IGNORED_FILES = [
@@ -24,14 +23,10 @@ const IGNORED_FILES = [
 const GLOBALS = {
   ...globals.browser,
   ...globals.node, // Include Node.js globals
-  ...globals.mocha,
 };
 
 const ESLINT_RULES = {
   ...eslintConfigPrettier.rules,
-  "mocha/no-exclusive-tests": "error",
-  "mocha/no-skipped-tests": "warn",
-  "mocha/no-hooks-for-single-case": "warn",
   "indent": "off", // Turn off the 'indent' rule
   "quotes": "off", // Turn off the 'quotes' rule
   "semi": "off", // Turn off the 'semi' rule
@@ -42,7 +37,6 @@ export default [
   {
     files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
     ignores: IGNORED_FILES,
-    plugins: { mocha },
     languageOptions: {
       globals: GLOBALS,
       ecmaVersion: "latest", // Auto-upgrade ECMAScript version
